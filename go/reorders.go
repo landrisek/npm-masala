c4d5dd51160781e3c4daa62f1655b93a61951f01
Lubomir Andrisek <landrisek@gmail.com>
2018-02-01 17:24:21 +0100

    [go]

--- go/reorders.go
+++ go/reorders.go
@@ -7,5 +7,5 @@ func main() {
 	input.Filters = map[string]interface{}{"producers_id":[]string{"_2"},"reorder":"clicked"}
 	input.Sort = []string{}
 	input.Status = "service"
-	masala.Grid{}.Inject(input, "http://10.10.0.100/4camping.cz/lubo/sklad/cron/reorders?key=IJlJtMv3qh0caFpY").Prepare()
+	masala.Grid{}.Inject(input, "testUrl").Prepare()
 }
