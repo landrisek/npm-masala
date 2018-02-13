82808809497d11c501c633ca443ed7d0d799eb5a
Lubomir Andrisek <landrisek@gmail.com>
2018-02-13 09:05:48 +0100

    [fix update]

--- react/Grid.jsx
+++ react/Grid.jsx
@@ -1005,7 +1005,8 @@ export default class Grid extends Component {
         } else {
             state[ROWS][event.target.name][event.target.id].Attributes.value = event.target.value
         }
-        state[ROWS][event.target.name] = JSON.parse(request('POST', this.state[BUTTONS].update, { json: {Row:state[ROWS][event.target.name],Submit:false}}).getBody('utf8'))
+        console.log(event.target.id)
+        state[ROWS][event.target.name] = JSON.parse(request('POST', this.state[BUTTONS].update, { json: {Key:event.target.id,Row:state[ROWS][event.target.name],Submit:false}}).getBody('utf8'))
         this.setState(state)
     }
 
